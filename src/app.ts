776d661568227652290853f15896ce610bc9ec60
Saleh Montazeran <skytask61@gmail.com>
2024-07-12 18:32:14 +0330

    Test linkedin

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
-import { ChatOpenAI } from '@langchain/openai'
+// import { ChatOpenAI } from '@langchain/openai'
 import 'dotenv/config'
 
-;(async () => {
-    const chatModel = new ChatOpenAI({
-        apiKey: process.env.OPENAI__API_KEY,
-        model: 'gpt-3.5-turbo',
-    })
+import { scrape_linkedin_profile } from './third_parties/linkedin'
 
-    const r = await chatModel.invoke('What are you?')
-    console.log(r.content)
+;(async () => {
+    // const chatModel = new ChatOpenAI({
+    //     apiKey: process.env.OPENAI__API_KEY,
+    //     model: 'gpt-3.5-turbo',
+    // })
+    // const r = await chatModel.invoke('What are you?')
+    // console.log(r.content)
+
+    const r = await scrape_linkedin_profile('linkedin.com/in/salehmontazeran')
+    console.log(r)
 })()
