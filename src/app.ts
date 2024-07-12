dd36ce4c2653b97bc19a1dedd4cc43cca4d87271
Saleh Montazeran <skytask61@gmail.com>
2024-07-12 16:52:03 +0330

    hello langchain

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log('Hi from Saleh')
+import { ChatOpenAI } from '@langchain/openai'
+import 'dotenv/config'
+
+;(async () => {
+    const chatModel = new ChatOpenAI({
+        apiKey: process.env.OPENAI__API_KEY,
+        model: 'gpt-3.5-turbo',
+    })
+
+    const r = await chatModel.invoke('What are you?')
+    console.log(r.content)
+})()
